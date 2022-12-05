80ef72631b46a0f15e5459a3da4992313db6c31a
Eshy10 <adewalebose15@gmail.com>
2022-12-05 22:59:59 +0100

    chore: add tailwind configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import { store } from '../src/redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<Router>
+				<App />
+			</Router>
+		</Provider>
 	</React.StrictMode>
 );
