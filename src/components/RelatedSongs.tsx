f3315bc8a889af6f481f7fd403570f4b9257341a
Eshy10 <adewalebose15@gmail.com>
2022-12-14 03:46:33 +0100

    chore: add related songs component

--- src/components/RelatedSongs.tsx
+++ src/components/RelatedSongs.tsx
@@ -1,3 +1,42 @@
-const RelatedSongs = () => <div>Loader</div>;
+import SongBar from './SongBar';
+import { Song } from '../redux/services/types/song';
+
+interface RelatedSongsProps {
+	data: any;
+	artistId: string;
+	isPlaying: boolean;
+	activeSong: any;
+	song: any;
+	handlePauseClick: () => void;
+	handlePlayClick: (song: any, i: number) => void;
+}
+
+const RelatedSongs = ({
+	data,
+	artistId,
+	isPlaying,
+	activeSong,
+	handlePauseClick,
+	handlePlayClick,
+}: RelatedSongsProps) => (
+	<div className="flex flex-col">
+		<h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+
+		<div className="mt-6 w-full flex flex-col">
+			{data?.map((song: Song, i: number) => (
+				<SongBar
+					key={`${artistId}-${song.key}-${i}`}
+					song={song}
+					i={i}
+					artistId={artistId}
+					isPlaying={isPlaying}
+					activeSong={activeSong}
+					handlePauseClick={handlePauseClick}
+					handlePlayClick={handlePlayClick}
+				/>
+			))}
+		</div>
+	</div>
+);
 
 export default RelatedSongs;
