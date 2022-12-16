a23d9be7c3c444d6d3890b0eb09975eadf91ad82
Eshy10 <adewalebose15@gmail.com>
2022-12-16 04:14:18 +0100

    chore: add around you component

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_SHAZAM_CORE_RAPID_API_KEY: string;
+	readonly VITE_GEO_API_KEY: string;
+	// more env variables...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
